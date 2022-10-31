8953db05c873deb583623855537760c4016f9d28
adt1710 <adrianoibira.adt@gmail.com>
2022-10-31 08:20:27 -0300

    Definição de uma frase do filme Vingadores.

---
+++ vingadores.py
@@ -0,0 +1,3 @@
+def mad_libs(verbo1,verbo2,substantivo1,substantivo2):
+    print( f"A {substantivo1} e a vingança, a fúria, a perda, o {substantivo2}, são todos motivadores tremendos. " \
+           f"Eles {verbo1} limpar sua mente, por isso estou pronto para {verbo2}. (Thor)")
