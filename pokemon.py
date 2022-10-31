4dd64de32feb381eb66b46eb7e782ad7c29fea75
adt1710 <adrianoibira.adt@gmail.com>
2022-10-31 08:20:10 -0300

    Definição de uma frase do anime Pokémon.

---
+++ pokemon.py
@@ -0,0 +1,6 @@
+def mad_libs(verbo1,verbo2,substantivo1,substantivo2):
+    print( f'Para {verbo1} as pessoas de nossa nação! ' \
+           f'Para {verbo2} os males da verdade e do amor! ' \
+           f'Para estender o nosso poder as {substantivo1}! Jessie! James! ' \
+           f'Equipe Rocket decolando na {substantivo2} da luz! ' \
+           f'Renda-se agora ou preparem-se para lutar!')
