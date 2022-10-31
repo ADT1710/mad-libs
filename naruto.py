96543fa184e5864e3391b88455162859151d8fcd
adt1710 <adrianoibira.adt@gmail.com>
2022-10-31 08:19:57 -0300

    Definição de uma frase do anime Naruto.

---
+++ naruto.py
@@ -0,0 +1,3 @@
+def mad_libs(verbo1,verbo2,substantivo1,substantivo2):
+    print( f"Cada um de nós deve {verbo1} o que está ao seu {substantivo1}! " \
+           f"Se vamos {verbo2} de qualquer maneira, então é melhor morrer lutando do que não fazer {substantivo2}!")
